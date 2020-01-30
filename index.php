445fe79fee609665fc04ab39bc0c8c47ee8d6bf4
wlcountrychick11 <60484092+wlcountrychick11@users.noreply.github.com>
2020-01-30 14:28:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php
-header("Location: https://www.chase.com/news/103117-chargebacks2");
-die();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Refresh" content="5;url=https://www.chase.com/news/103117-chargebacks2">
+</head>
